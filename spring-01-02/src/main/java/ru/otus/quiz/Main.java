5ee2474dbab78ee07e7c39350a1540f1f7d97d45
Dmitrii Kosurov <dkosurov@diasoft.ru>
2024-11-22 18:37:39 +0300

    refactor

--- spring-01-02/src/main/java/ru/otus/quiz/Main.java
+++ spring-01-02/src/main/java/ru/otus/quiz/Main.java
@@ -3,6 +3,7 @@ package ru.otus.quiz;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+import ru.otus.quiz.service.QuizService;
 import ru.otus.quiz.service.impl.QuizServiceImpl;
 
 @PropertySource("classpath:application.properties")
@@ -11,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
-        QuizServiceImpl testService = context.getBean(QuizServiceImpl.class);
-        testService.start();
+        QuizService quizService = context.getBean(QuizServiceImpl.class);
+        quizService.start();
     }
 }
